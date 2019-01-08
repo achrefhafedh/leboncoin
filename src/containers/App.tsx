9e6a6c620f8edd67c7516ef9bf787bd72d6f974a
Achref Hafedh <achref@abtasty.com>
2019-01-08 01:28:12 +0100

    config redux

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+
+import { configureStore } from '../store/configure-store';
 import './App.scss';
 import { PageLayout } from 'modules/page-layout';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
       <>
-        <PageLayout />
+        <Provider store={store}>
+          <PageLayout />
+        </Provider>
       </>
     );
   }
--- src/modules/login/index.ts
+++ src/modules/login/index.ts
@@ -1,1 +1,12 @@
+import { Reducer } from 'redux';
+import { registerModuleReducer } from 'redux-register-module';
+
+import { loginReducer } from './redux/reducer';
+
 export { Auth } from './containers';
+
+export const registerLogin = () => {
+  registerModuleReducer('login', loginReducer as Reducer<any>);
+};
+
+export * from './redux/reducer';
